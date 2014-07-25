770de798cc7b0cb91ca4e41d7719e264bb012bc8
akmal80 <akmal80@users.noreply.github.com>
2014-07-25 00:13:05 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,4 @@
+## by Akmal Al-Fergani
 ## Put comments here that give an overall description of what your
 ## functions do
 
@@ -5,11 +6,43 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
-}
+  if (!is.matrix(x)) {
+    warning("argument is not a matrix: returning NA")
+    return(NA_real_)
+  }
+  
+  if (nrow(x) != ncol(x)) {
+    warning("argument is not a squire matrix")
+    return(x)
+  }
+  
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setSolve <- function(solve) s <<- solve
+  getSolve <- function() s
+  list(set = set, get = get,
+       setSolve = setSolve,
+       getSolve = getSolve)
 
+}
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cachSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+  s <- x$getSolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setSolve(s)
+  s
+
 }
